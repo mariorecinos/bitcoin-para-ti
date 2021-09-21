dec56406b5070fe314141cee977355027d3df59e
Mario <recinos.mario68@gmail.com>
2021-09-20 23:30:32 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+$(function() {
+// Constants and Variables to reference Data
+const BASE_URL = 'https://api.coinbase.com/v2/prices/';
+let cryptoData;
+
+// Cached Element References
+const $crypto = $('#crypto');
+const $current = $('#current');
+const $fiat = $('#fiat');
+const $form = $('form');
+const $cryinput = $('#csymb[type="text"]');
+const $fiatinput = $('#fsymb[type="text"]');
+
+// Event Listener
+$form.on('submit', handleGetData)
+
+// function to get Data using Javascript
+function handleGetData(event) {
+
+  event.preventDefault();
+  // setting up input variable
+  const $cryptoName = $cryinput.val();
+  const $fiatName = $fiatinput.val();
+  console.log($fiatName);
+  // this method is used to keep the input blank after input entered
+  $cryinput.val("");
+  $fiatinput.val("");
+  // ajax call request
+  $.ajax(`${BASE_URL}${$cryptoName}-${$fiatName}/spot?`).then(function (data) {
+    // referencing data in parameter of function line 28
+    cryptoData = data;
+    console.log($fiatName);
+
+    // if successful then render function is called
+    render ();
+    // if an error than will console.log error
+  }, function (error) {
+    console.log(error);
+  });
+
+  }
+
+  // function that will display the data requested in text
+  function render() {
+    $crypto.text(cryptoData.data.base); // cryptocurrency
+    $current.text(cryptoData.data.amount); // current price
+    $fiat.text(cryptoData.data.currency); // fiat currency
+  }
+
+  });
